978ff4bf915e0274272c8bdf7b97cf27fd300b01
David Meister <thedavidmeister@gmail.com>
2017-07-30 13:07:24 +1000

    from dim valley

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+echo "checking out master...";
+git checkout master;
+
+echo "fetching from git...";
+git fetch;
+
+echo "building gh-pages directory...";
 boot gh-pages;
-git add gh-pages;
-git commit -am 'Deploying to gh-pages';
-git subtree push --prefix gh-pages origin gh-pages;
+
+echo "committing gh-pages directory to git..."
+git add .;
+git commit -am "Deploy to github pages";
+
+echo "deploying to remote gh-pages branch..."
+# git subtree push --prefix gh-pages origin gh-pages;
+# https://stackoverflow.com/questions/13756055/git-subtree-subtree-up-to-date-but-cant-push/30715198#30715198
+git push origin `git subtree split --prefix gh-pages master`:gh-pages --force;
+
+# Keep origin/master up to date as well
+echo "pushing to master..."
+git push;
